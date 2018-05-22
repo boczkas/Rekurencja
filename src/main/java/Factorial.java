5188766d53be7d002de75839e000444e3104df47
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-22 16:27:41 +0200

    First 2 exercises done

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,41 @@
+public class Factorial {
+    public static void main(String[] args) {
+//        System.out.println(factorial(3));
+//        System.out.println(bunnyEars(1));
+        System.out.println(fibonacci(5));
+    }
+
+    private static int fibonacci(int n) {
+        if(n == 0){
+            return 0;
+        }
+
+        if(n == 1){
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    private static int bunnyEars(int bunny) {
+        if(bunny == 0){
+            return 0;
+        }
+
+        int count = 2;
+        return count + bunnyEars(bunny - 1);
+    }
+
+    private static int factorial(int n) {
+        int result;
+
+        if(n == 1){
+            return 1;
+        }
+
+        result = factorial(n - 1) * n;
+        return result;
+    }
+
+
+}
